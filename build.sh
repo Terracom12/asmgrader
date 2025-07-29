7fd191718410d3648c2a743e4111435d8f54e374
Matthew Reese <awesomepop012@hotmail.com>
2025-07-28 17:09:15 -0700

    Allow passing of build args to `build.sh` script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+BUILD_ARGS=()
+if [[ $# -gt 0 ]]; then
+    BUILD_ARGS=("${BUILD_ARGS[@]}" "$@")
+fi
+
 cmake -B build -S . -DENABLE_TESTING:BOOL:=TRUE -DCMAKE_BUILD_TYPE:STRING:=Debug -DFETCHCONTENT_UPDATES_DISCONNECTED:BOOL:=TRUE
-cmake --build build
+cmake --build build "${BUILD_ARGS[@]}"
