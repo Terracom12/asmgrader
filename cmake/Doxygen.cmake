56c98953effff20dee2914f57fe703fe8ad2df23
Matthew Reese <awesomepop012@hotmail.com>
2025-08-29 16:50:27 -0700

    Ensure generated "version.hpp" is added to doxygen docs [no ci]

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -94,6 +94,7 @@ function(asmgrader_enable_doxygen DOXYGEN_THEME)
     message(STATUS "Adding `doxygen-docs` target that builds the documentation.")
     doxygen_add_docs(
         doxygen-docs 
+        "${CMAKE_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}/asmgrader/version.hpp"
         ALL 
         ${PROJECT_SOURCE_DIR}
         COMMENT "Generating documentation - entry file: ${CMAKE_CURRENT_BINARY_DIR}/html/index.html"
