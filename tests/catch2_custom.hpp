5cd34a9d078128114ed7988fdfaea9259f7ac44d
Matthew Reese <awesomepop012@hotmail.com>
2025-08-30 23:46:32 -0700

    Add formatters include to catch2_custom

--- tests/catch2_custom.hpp
+++ tests/catch2_custom.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "catch2/catch_tostring.hpp"
+#include "common/formatters/formatters.hpp" // IWYU pragma: keep
 
 #include <catch2/catch_test_macros.hpp> // IWYU pragma: export
+#include <catch2/catch_tostring.hpp>
 #include <fmt/base.h>
 #include <fmt/format.h>
 
@@ -12,7 +13,7 @@ namespace Catch {
 
 template <fmt::formattable T>
 // inverse of Catch2's conditions as to not cause ambiguity
-    requires(!is_range<T>::value && !::Catch::Detail::IsStreamInsertable<T>::value)
+    requires(!is_range<T>::value && !Catch::Detail::IsStreamInsertable<T>::value)
 struct StringMaker<T>
 {
     static std::string convert(const T& t) { return fmt::format("{}", t); }
