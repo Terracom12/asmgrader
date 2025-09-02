af02c25d314764ae8a7f6a6e867d332ec3877610
Matthew Reese <awesomepop012@hotmail.com>
2025-09-01 17:20:26 -0700

    Add macros to api/test_macros for Aarch64 registers; update main library header
    Add essentially every include we might want in test impl sources to main
    library header (include/asmgrader/asmgrader.hpp).
    
    May run into issues down the line with the short macro names in
    test_macros conflicting with other libraries. For now, just try to
    include last; Or, even better, don't include any headers other than
    asmgrader.hpp.

--- include/asmgrader/api/test_macros.hpp
+++ include/asmgrader/api/test_macros.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+// Macro names in this file are short and may conflict with identifiers in 3rd-party libraries
+// known conflicts with range-v3, so we just make sure to include it before
+#include <range/v3/all.hpp> // IWYU pragma: export
+//
+
 #include <asmgrader/api/assignment.hpp> // IWYU pragma: export
 #include <asmgrader/api/test_base.hpp>  // IWYU pragma: export
 #include <asmgrader/api/test_context.hpp>
@@ -7,6 +12,8 @@
 #include <asmgrader/grading_session.hpp> // IWYU pragma: export
 #include <asmgrader/logging.hpp>
 #include <asmgrader/registrars/auto_registrars.hpp> // IWYU pragma: export
+
+#include <boost/preprocessor/repetition/repeat.hpp>
 
 // Some macros to substantially simplify test case development
 
@@ -74,6 +81,81 @@
 
 // Macros for registers and flags
 
-#define REGS_MACRO_IMPL(ident) ctx.get_registers().ident()
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define REGS_MACRO_IMPL(which) ctx.get_registers().which
 
-#define X0 ctx.get_registers
+#define W0 REGS_MACRO_IMPL(w0)
+#define X0 REGS_MACRO_IMPL(x0)
+#define W1 REGS_MACRO_IMPL(w1)
+#define X1 REGS_MACRO_IMPL(x1)
+#define W2 REGS_MACRO_IMPL(w2)
+#define X2 REGS_MACRO_IMPL(x2)
+#define W3 REGS_MACRO_IMPL(w3)
+#define X3 REGS_MACRO_IMPL(x3)
+#define W4 REGS_MACRO_IMPL(w4)
+#define X4 REGS_MACRO_IMPL(x4)
+#define W5 REGS_MACRO_IMPL(w5)
+#define X5 REGS_MACRO_IMPL(x5)
+#define W6 REGS_MACRO_IMPL(w6)
+#define X6 REGS_MACRO_IMPL(x6)
+#define W7 REGS_MACRO_IMPL(w7)
+#define X7 REGS_MACRO_IMPL(x7)
+#define W8 REGS_MACRO_IMPL(w8)
+#define X8 REGS_MACRO_IMPL(x8)
+#define W9 REGS_MACRO_IMPL(w9)
+#define X9 REGS_MACRO_IMPL(x9)
+#define W10 REGS_MACRO_IMPL(w10)
+#define X10 REGS_MACRO_IMPL(x10)
+#define W11 REGS_MACRO_IMPL(w11)
+#define X11 REGS_MACRO_IMPL(x11)
+#define W12 REGS_MACRO_IMPL(w12)
+#define X12 REGS_MACRO_IMPL(x12)
+#define W13 REGS_MACRO_IMPL(w13)
+#define X13 REGS_MACRO_IMPL(x13)
+#define W14 REGS_MACRO_IMPL(w14)
+#define X14 REGS_MACRO_IMPL(x14)
+#define W15 REGS_MACRO_IMPL(w15)
+#define X15 REGS_MACRO_IMPL(x15)
+#define W16 REGS_MACRO_IMPL(w16)
+#define X16 REGS_MACRO_IMPL(x16)
+#define W17 REGS_MACRO_IMPL(w17)
+#define X17 REGS_MACRO_IMPL(x17)
+#define W18 REGS_MACRO_IMPL(w18)
+#define X18 REGS_MACRO_IMPL(x18)
+#define W19 REGS_MACRO_IMPL(w19)
+#define X19 REGS_MACRO_IMPL(x19)
+#define W20 REGS_MACRO_IMPL(w20)
+#define X20 REGS_MACRO_IMPL(x20)
+#define W21 REGS_MACRO_IMPL(w21)
+#define X21 REGS_MACRO_IMPL(x21)
+#define W22 REGS_MACRO_IMPL(w22)
+#define X22 REGS_MACRO_IMPL(x22)
+#define W23 REGS_MACRO_IMPL(w23)
+#define X23 REGS_MACRO_IMPL(x23)
+#define W24 REGS_MACRO_IMPL(w24)
+#define X24 REGS_MACRO_IMPL(x24)
+#define W25 REGS_MACRO_IMPL(w25)
+#define X25 REGS_MACRO_IMPL(x25)
+#define W26 REGS_MACRO_IMPL(w26)
+#define X26 REGS_MACRO_IMPL(x26)
+#define W27 REGS_MACRO_IMPL(w27)
+#define X27 REGS_MACRO_IMPL(x27)
+#define W28 REGS_MACRO_IMPL(w28)
+#define X28 REGS_MACRO_IMPL(x28)
+#define W29 REGS_MACRO_IMPL(w29)
+#define X29 REGS_MACRO_IMPL(x29)
+#define W30 REGS_MACRO_IMPL(w30)
+#define X30 REGS_MACRO_IMPL(x30)
+
+#define FP REGS_MACRO_IMPL(fp)
+#define LR REGS_MACRO_IMPL(lr)
+#define SP REGS_MACRO_IMPL(sp)
+#define PC REGS_MACRO_IMPL(pc)
+
+#define PSTATE REGS_MACRO_IMPL(pstate)
+#define N PSTATE.n()
+#define Z PSTATE.z()
+#define C PSTATE.c()
+#define V PSTATE.v()
+
+// TODO: x86-64 accessors
--- include/asmgrader/asmgrader.hpp
+++ include/asmgrader/asmgrader.hpp
@@ -1,11 +1,46 @@
 #pragma once
 
-#include <asmgrader/api/asm_buffer.hpp>   // IWYU pragma: export
-#include <asmgrader/api/asm_data.hpp>     // IWYU pragma: export
-#include <asmgrader/api/asm_function.hpp> // IWYU pragma: export
-#include <asmgrader/api/asm_symbol.hpp>   // IWYU pragma: export
-#include <asmgrader/api/test_base.hpp>    // IWYU pragma: export
-#include <asmgrader/api/test_context.hpp> // IWYU pragma: export
-#include <asmgrader/api/test_macros.hpp>  // IWYU pragma: export
-#include <asmgrader/common/aliases.hpp>   // IWYU pragma: export
-#include <asmgrader/common/bit_casts.hpp> // IWYU pragma: export
+#include <asmgrader/api/asm_buffer.hpp>               // IWYU pragma: export
+#include <asmgrader/api/asm_data.hpp>                 // IWYU pragma: export
+#include <asmgrader/api/asm_function.hpp>             // IWYU pragma: export
+#include <asmgrader/api/asm_symbol.hpp>               // IWYU pragma: export
+#include <asmgrader/api/test_base.hpp>                // IWYU pragma: export
+#include <asmgrader/api/test_context.hpp>             // IWYU pragma: export
+#include <asmgrader/common/aliases.hpp>               // IWYU pragma: export
+#include <asmgrader/common/bit_casts.hpp>             // IWYU pragma: export
+#include <asmgrader/common/formatters/formatters.hpp> // IWYU pragma: export
+#include <asmgrader/logging.hpp>                      // IWYU pragma: export
+
+#include <fmt/format.h> // IWYU pragma: export
+
+// Include just about every stdlib headers that we might possibly want for convenience,
+// and to make test sources less visually cluttered
+#include <algorithm>   // IWYU pragma: export
+#include <array>       // IWYU pragma: export
+#include <cerrno>      // IWYU pragma: export
+#include <charconv>    // IWYU pragma: export
+#include <compare>     // IWYU pragma: export
+#include <concepts>    // IWYU pragma: export
+#include <csignal>     // IWYU pragma: export
+#include <cstddef>     // IWYU pragma: export
+#include <cstdio>      // IWYU pragma: export
+#include <cstdlib>     // IWYU pragma: export
+#include <ctime>       // IWYU pragma: export
+#include <filesystem>  // IWYU pragma: export
+#include <functional>  // IWYU pragma: export
+#include <iostream>    // IWYU pragma: export
+#include <limits>      // IWYU pragma: export
+#include <memory>      // IWYU pragma: export
+#include <numeric>     // IWYU pragma: export
+#include <optional>    // IWYU pragma: export
+#include <regex>       // IWYU pragma: export
+#include <string>      // IWYU pragma: export
+#include <string_view> // IWYU pragma: export
+#include <tuple>       // IWYU pragma: export
+#include <type_traits> // IWYU pragma: export
+#include <utility>     // IWYU pragma: export
+#include <variant>     // IWYU pragma: export
+#include <vector>      // IWYU pragma: export
+
+// should always include last if possible, as the short macro names may conflict with other libraries.
+#include <asmgrader/api/test_macros.hpp> // IWYU pragma: export
