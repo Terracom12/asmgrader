c1ce0fa7725efd1c507a14e1c3899d3f544bf71c
Matthew Reese <awesomepop012@hotmail.com>
2025-09-17 14:36:36 -0700

    fix: static_size now works with non-range-like types

--- include/asmgrader/meta/static_size.hpp
+++ include/asmgrader/meta/static_size.hpp
@@ -15,12 +15,17 @@ concept HasStaticSize = requires {
     { std::tuple_size<std::decay_t<Range>>::value } -> std::convertible_to<std::size_t>;
 };
 
-template <ranges::range Range>
+template <typename Range>
 consteval std::size_t get_static_size() {
-    return std::tuple_size_v<std::decay_t<Range>>;
+    using Type = std::remove_cvref_t<Range>;
+    if constexpr (std::is_array_v<Type>) {
+        return std::extent_v<Type, 0>;
+    } else {
+        return std::tuple_size_v<Type>;
+    }
 }
 
-template <ranges::range Range>
+template <typename Range>
 constexpr std::size_t get_static_size_or(std::size_t default_value) {
     if constexpr (HasStaticSize<Range>) {
         return get_static_size<Range>();
