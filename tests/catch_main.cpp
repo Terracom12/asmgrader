9e6735b471046b76c060c87520760e187880131b
Matthew Reese <awesomepop012@hotmail.com>
2025-09-19 14:48:59 -0700

    Add libassert failure handler for testing executables that throws exception instead of crashing

--- tests/catch_main.cpp
+++ tests/catch_main.cpp
@@ -1,9 +1,20 @@
 #include "logging.hpp"
 
 #include <catch2/catch_session.hpp>
+#include <libassert/assert.hpp>
+
+#include <stdexcept>
+
+void libassert_handler(const libassert::assertion_info& info) {
+    LOG_ERROR(info.to_string());
+
+    throw std::runtime_error(info.to_string());
+}
 
 int main(int argc, char* argv[]) {
     asmgrader::init_loggers();
+
+    libassert::set_failure_handler(libassert_handler);
 
     int result = Catch::Session().run(argc, argv);
 
