d58cfa4f2a28be7306aae445e938b8bff1662861
Matthew Reese <awesomepop012@hotmail.com>
2025-09-27 14:26:34 -0700

    Add tests for a database with crlf line breaks

--- tests/test_database_reader.cpp
+++ tests/test_database_reader.cpp
@@ -22,6 +22,8 @@ TEST_CASE("Read small database") {
         {"Matthew", "Reese"},
         {"John", "Doe"},
         {"Pope", "Francis"},
+        {"Jack Bryan", "O'Reily"},
+        {"Carlos", "De La Cruz"},
     });
 
     for (const auto& [expected_name, read_name] : ranges::views::zip(EXPECTED_NAMES, *res)) {
@@ -39,4 +41,26 @@ TEST_CASE("Read non-existant database; ensure error") {
     asmgrader::DatabaseReader reader{path{RESOURCES_DIR} / "i-do-not-exist.csv"};
 
     REQUIRE_FALSE(reader.read());
+}
+
+TEST_CASE("Read a database with CRLF line breaks") {
+    // this database actually has MIXED line breaks: CRLF for some and just LF for others
+    asmgrader::DatabaseReader reader{path{RESOURCES_DIR} / "crlf_database.csv"};
+
+    auto res = reader.read();
+
+    REQUIRE(res);
+
+    constexpr std::array EXPECTED_NAMES = std::to_array<std::tuple<std::string_view, std::string_view>>({
+        {"Matthew", "Reese"},
+        {"John", "Doe"},
+        {"Jane", "Doe"},
+        {"Pope", "Francis"},
+        {"Jack Bryan", "O'Reily"},
+        {"Carlos", "De La Cruz"},
+    });
+
+    for (const auto& [expected_name, read_name] : ranges::views::zip(EXPECTED_NAMES, *res)) {
+        REQUIRE(std::tie(read_name.first_name, read_name.last_name) == expected_name);
+    }
 }
