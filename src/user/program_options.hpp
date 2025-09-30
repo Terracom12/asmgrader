6216b718e38b23a41ce79f852af074ccb7fc92ed
Matthew Reese <awesomepop012@hotmail.com>
2025-09-30 12:37:19 -0700

    move static definitions from program_options.hpp to a source file

--- src/user/program_options.hpp
+++ src/user/program_options.hpp
@@ -3,10 +3,8 @@
 #include <asmgrader/common/formatters/debug.hpp>
 #include <asmgrader/registrars/global_registrar.hpp>
 
-#include "common/error_types.hpp"
 #include "common/expected.hpp"
 #include "output/verbosity.hpp"
-#include "program/program.hpp"
 #include "user/assignment_file_searcher.hpp"
 #include "version.hpp"
 
@@ -15,14 +13,10 @@
 #include <fmt/format.h>
 #include <libassert/assert.hpp>
 
-#include <algorithm>
-#include <exception>
 #include <filesystem>
 #include <optional>
-#include <regex>
 #include <string>
 #include <string_view>
-#include <tuple>
 
 namespace asmgrader {
 
@@ -62,76 +56,16 @@ struct ProgramOptions
     static constexpr auto DEFAULT_VERBOSITY_LEVEL = VerbosityLevel::Summary;
 
     static Expected<void, std::string> ensure_file_exists(const std::filesystem::path& path,
-                                                          fmt::format_string<std::string> fmt) {
-        if (!std::filesystem::exists(path)) {
-            return (fmt::format(fmt, path.string()) + " does not exist");
-        }
-
-        return {};
-    }
+                                                          fmt::format_string<std::string> fmt);
 
     static Expected<void, std::string> ensure_is_regular_file(const std::filesystem::path& path,
-                                                              fmt::format_string<std::string> fmt) {
-        TRY(ensure_file_exists(path, fmt));
-
-        if (!std::filesystem::is_regular_file(path)) {
-            return (fmt::format(fmt, path.string()) + " is not a regular file");
-        }
-
-        return {};
-    }
+                                                              fmt::format_string<std::string> fmt);
 
     [[maybe_unused]] static Expected<void, std::string> ensure_is_directory(const std::filesystem::path& path,
-                                                                            fmt::format_string<std::string> fmt) {
-        TRY(ensure_file_exists(path, fmt));
-
-        if (!std::filesystem::is_directory(path)) {
-            return (fmt::format(fmt, path.string()) + " is not a directory");
-        }
-
-        return {};
-    }
+                                                                            fmt::format_string<std::string> fmt);
 
     /// Verify that all fields are valid
-    Expected<void, std::string> validate() {
-        // Assume that all enumerators have valid values except for verbosity
-        // which we will just clamp to [MIN, MAX]
-
-        constexpr auto MAX_VERBOSITY = VerbosityLevel::Max;
-        constexpr auto MIN_VERBOSITY = VerbosityLevel{};
-
-        verbosity = std::clamp(verbosity, MIN_VERBOSITY, MAX_VERBOSITY);
-
-        // Ensure that the matcher is a valid RegEx
-        try {
-            std::ignore = std::regex{file_matcher};
-        } catch (std::exception& ex) {
-            return (fmt::format("File matcher {:?} is invalid. {}", file_matcher, ex.what()));
-        }
-
-        TRY(ensure_is_directory(search_path, "Search path {:?}"));
-
-        // The CLI should verify that the specified assignment is valid
-        // We'll check here just in case and return an error if it's not
-        auto assignment = TRYE(GlobalRegistrar::get().get_assignment(assignment_name),
-                               fmt::format("Error locating assignment {}", assignment_name));
-
-        if (APP_MODE != AppMode::Professor) {
-            // TODO: A more friendly diagnostic for non-existant file
-            std::string exec_file_name = file_name.value_or(assignment.get().get_exec_path());
-
-            TRY(ensure_is_regular_file(exec_file_name, "File to run tests on {:?}"));
-            TRY(Program::check_is_compat_elf(exec_file_name));
-        }
-
-        // Only check the database path if it's not the default
-        // non-existance will be handled properly in ProfessorApp
-        if (database_path != DEFAULT_DATABASE_PATH) {
-            TRY(ensure_is_regular_file(database_path, "Database file {:?}"));
-        }
-
-        return {};
-    }
+    Expected<void, std::string> validate();
 };
 
 } // namespace asmgrader
