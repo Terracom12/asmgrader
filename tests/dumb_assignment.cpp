c4cb18c8fdb5dd856f95e5e0af34ec2bf344ee0d
Matthew Reese <awesomepop012@hotmail.com>
2025-10-22 17:15:40 -0700

    fix: deprecation warnings (gcc)

--- tests/dumb_assignment.cpp
+++ tests/dumb_assignment.cpp
@@ -14,30 +14,30 @@ TEST("sum function") {
     AsmFunction sum = ctx.find_function<u64(u64, u64)>("putch");
     AsmFunction sum_and_write = ctx.find_function<void(u64, u64)>("sum_and_write");
 
-    REQUIRE(sum(0, 0) == 0);
-    REQUIRE(sum(1, 1) == 2);
-    REQUIRE(sum(10000, 9999) == 19999);
+    REQUIRE(sum(0, 0) == 0, "0 + 0 = 0");
+    REQUIRE(sum(1, 1) == 2, "1 + 1 = 2");
+    REQUIRE(sum(10000, 9999) == 19999, "10000 + 9999 = 19999");
 
-    REQUIRE(sum_and_write(0xAB, 0xCD));
-    REQUIRE(ctx.get_stdout() == "\xAB\0\0\0\0\0\0\0\0\xCD\0\0\0\0\0\0\0\0");
+    REQUIRE(sum_and_write(0xAB, 0xCD), "function runs!");
+    REQUIRE(ctx.get_stdout() == "\xAB\0\0\0\0\0\0\0\0\xCD\0\0\0\0\0\0\0\0", "stdout's cool");
 
-    REQUIRE(sum_and_write(0x0123456789ABCDEF, 0xFEDCBA9876543210));
-    REQUIRE(ctx.get_stdout() == "\x01\x23\x45\x67\x89\xAB\xCD\xEF\xFE\xDC\xBA\x98\x76\x54\x32\x10");
+    REQUIRE(sum_and_write(0x0123456789ABCDEF, 0xFEDCBA9876543210), "function runs!");
+    REQUIRE(ctx.get_stdout() == "\x01\x23\x45\x67\x89\xAB\xCD\xEF\xFE\xDC\xBA\x98\x76\x54\x32\x10", "stdout's cool");
 
     auto syscalls = ctx.get_syscall_records();
-    REQUIRE(syscalls.size() == 2);
+    REQUIRE(syscalls.size() == 2, "2 syscalls!");
 }
 
 TEST("exiting_fn") {
     AsmFunction exiting_fn = ctx.find_function<void(u64)>("putch");
 
-    REQUIRE(exiting_fn(0) == ErrorKind::UnexpectedReturn);
+    REQUIRE(exiting_fn(0) == ErrorKind::UnexpectedReturn, "We exited");
 }
 
 TEST("symbols") {
     AsmSymbol strHello = ctx.find_symbol<std::string>("strHello");
     AsmSymbol strGoodbye = ctx.find_symbol<std::string>("strGoodbye");
 
-    REQUIRE(*strHello == "Hello, from assembly!\n");
-    REQUIRE(*strGoodbye == "Goodbye, :(\n");
+    REQUIRE(*strHello == "Hello, from assembly!\n", "symbol check");
+    REQUIRE(*strGoodbye == "Goodbye, :(\n", "~cymbal~ symbol check!");
 }
