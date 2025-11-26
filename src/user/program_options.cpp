053fe7169bff1b4d93820876670f6e4ad7c062f8
Matthew Reese <awesomepop012@hotmail.com>
2025-11-26 15:51:20 -0800

    fix: remove duplicate validate function from ProgramOptions

--- src/user/program_options.cpp
+++ src/user/program_options.cpp
@@ -50,44 +50,4 @@ Expected<void, std::string> ProgramOptions::ensure_is_directory(const std::files
     return {};
 }
 
-Expected<void, std::string> ProgramOptions::validate() {
-    // Assume that all enumerators have valid values except for verbosity
-    // which we will just clamp to [MIN, MAX]
-
-    constexpr auto max_verbosity = VerbosityLevel::Max;
-    constexpr auto min_verbosity = VerbosityLevel{};
-
-    verbosity = std::clamp(verbosity, min_verbosity, max_verbosity);
-
-    // Ensure that the matcher is a valid RegEx
-    try {
-        std::ignore = std::regex{file_matcher};
-    } catch (std::exception& ex) {
-        return (fmt::format("File matcher {:?} is invalid. {}", file_matcher, ex.what()));
-    }
-
-    TRY(ensure_is_directory(search_path, "Search path {:?}"));
-
-    // The CLI should verify that the specified assignment is valid
-    // We'll check here just in case and return an error if it's not
-    auto assignment = TRYE(GlobalRegistrar::get().get_assignment(assignment_name),
-                           fmt::format("Error locating assignment {}", assignment_name));
-
-    if (APP_MODE != AppMode::Professor) {
-        // TODO: A more friendly diagnostic for non-existant file
-        std::string exec_file_name = file_name.value_or(assignment.get().get_exec_path());
-
-        TRY(ensure_is_regular_file(exec_file_name, "File to run tests on {:?}"));
-        TRY(Program::check_is_compat_elf(exec_file_name));
-    }
-
-    // Only check the database path if it's not the default
-    // non-existance will be handled properly in ProfessorApp
-    if (database_path != DEFAULT_DATABASE_PATH) {
-        TRY(ensure_is_regular_file(database_path, "Database file {:?}"));
-    }
-
-    return {};
-}
-
 } // namespace asmgrader
